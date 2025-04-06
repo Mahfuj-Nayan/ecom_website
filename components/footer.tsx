cd0bc0736287537612a6708db71d4eabbb3af44c
Mahfuj-Nayan <minayan.up@gmail.com>
2025-04-06 11:27:24 +0600

    Small change

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="border-t">
       <div className="p-5 flex-center">
-        {currentYear} {APP_NAME}. All Right Reserved
+        {currentYear} {APP_NAME} by MI Nayan. All Right Reserved
       </div>
     </footer>
   );
