b450ff504ba58c1d8e93fd5d028b4d1dd8806d94
Mahfuj-Nayan <minayan.up@gmail.com>
2025-05-29 23:04:19 +0600

    warning fix

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -79,6 +79,7 @@ export async function getAllProducts({
           rating: { gte: Number(rating) },
         }
       : {};
+  // Sort filter
 
   const data = await prisma.product.findMany({
     where: {
@@ -87,7 +88,14 @@ export async function getAllProducts({
       ...priceFilter,
       ...ratingFilter,
     },
-    orderBy: { createdAt: "desc" },
+    orderBy:
+      sort === "lowest"
+        ? { price: "asc" }
+        : sort === "highest"
+        ? { price: "desc" }
+        : sort === "rating"
+        ? { rating: "desc" }
+        : { createdAt: "desc" },
     skip: (page - 1) * limit,
     take: limit,
   });
